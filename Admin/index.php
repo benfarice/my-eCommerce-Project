af9add0e1f95fa33cd730ffad5ead316a31a749f
benfarice <imzoughene@outlook.com>
2018-02-06 17:50:14 +0000

    Code The Login Form Part 1

--- Admin/index.php
+++ Admin/index.php
@@ -3,10 +3,31 @@
 	include $tpl."header.php";
 	//include 'includes/languages/arabic.php';
 	include 'includes/languages/english.php';
+
+
+
+	// Check if User Coming From HTTP Post Request
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+		$username = $_POST['user'];
+		$password = $_POST['pass'];
+		//echo $username .'  '.$password;
+		$hashedPass = sha1($password);
+		//echo $hashedPass;
+		// Check if the user exist in database
+		$stmt = $con->prepare("select username,password from users where username = ? and password = ? and GroupID = 1");
+		$stmt->execute(array($username,$hashedPass));
+		$count = $stmt->rowCount();
+		//echo $count;
+		// If count > 0 this mean the database contain record about this username
+		if($count > 0){
+			echo "welcome ".$username ;
+		}
+	}
 ?>
 	<!--Welcome to Index-->
 
-	<form class="login">
+	<form class="login" action="<?php echo $_SERVER['PHP_SELF'] ?>"
+		method="POST">
 		<h4 class="text-center">Admin Login</h4>
 		<input class="form-control" type="text" name="user" placeholder="Username" autocomplete="off" />
 		<input class="form-control" type="password" name="pass"
--- Admin/layout/js/backend.js
+++ Admin/layout/js/backend.js
@@ -1,5 +1,12 @@
 $(function(){
 	'use strict';
+	/*
 	// Hide Placeholder On Form Focus
+	$('[placeholder]').focus(funcion(){
+		$(this).attr('data-text',$(this).attr('placeholder'));
+		$(this).attr('placeholder','');
+	}).blur(function(){
+
+	});*/
 
 });
