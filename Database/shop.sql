438ef79273098b33dcd4c2fbc615cdd9163382c9
benfarice <imzoughene@outlook.com>
2018-02-06 17:55:29 +0000

    update database

--- Database/shop.sql
+++ Database/shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  mar. 06 fév. 2018 à 13:40
+-- Généré le :  mar. 06 fév. 2018 à 18:53
 -- Version du serveur :  10.1.29-MariaDB
 -- Version de PHP :  7.1.12
 
@@ -40,6 +40,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`UserID`, `UserName`, `Password`, `Email`, `FullName`, `GroupID`, `TrustStatus`, `RegStatus`) VALUES
+(1, 'imzoughene', '42dec965e694c48f1f5d7cafbe9d7b11fe7a3589', 'imzoughene@outlook.com', 'Youssef Imzoughene', 1, 0, 1);
+
+--
 -- Index pour les tables déchargées
 --
 
@@ -57,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'To Identify User ';
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'To Identify User ', AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
